cb48c816dd28c9ff298b7ad240360f35c139dad6
nickmask <nrmaskell@gmail.com>
2016-09-21 15:06:42 +1200

    Split SSR and CSR modes

--- webpack.dev.js
+++ webpack.dev.js
@@ -12,20 +12,24 @@ const atImport = require('postcss-import')
 
 const PATHS = {
   src: path.join(__dirname, 'src'),
-  app: path.join(__dirname, 'src', 'client', 'app.js'),
-  dist: path.join(__dirname, 'dist', 'js'),
+  app: path.join(__dirname, 'src/client/app.js'),
+  dist: path.join(__dirname, 'dist'),
+  client: path.join(__dirname, 'src', 'client'),
 }
 
 const webpackconfig = {
   devtool: 'eval-cheap-module-source-map',
   entry: {
     app: [
+      'react-hot-loader/patch', // Add react hot loader 3
+      'webpack-dev-server/client', // Webpack dev server
+      'webpack/hot/dev-server', // Webpack dev server auto refresh / hot loading
       PATHS.app,
     ],
   },
   output: {
     path: PATHS.dist,
-    filename: 'bundle.js', // Output name of bundle
+    filename: '[name].js', // Output name of bundle
     publicPath: '/',
   },
   module: {
@@ -37,7 +41,10 @@ const webpackconfig = {
         loader: 'babel',
         query: {
           cacheDirectory: true,
-          presets: ['es2015', 'react'],
+          presets: ['modern-browsers', 'react'],
+          plugins: [
+            'react-hot-loader/babel',
+          ],
         },
       },
       {
@@ -58,14 +65,31 @@ const webpackconfig = {
     modulevalues,
   ]),
   plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(PATHS.client, 'index.html'), // Use index.html as template for index.html
+      chunksSortMode: 'dependency', // Order the dependacy so that bundle comes first
+      filename: 'index.html', // Output file name
+      inject: 'body', // Enject into the end of the body tag
+    }),
+    new webpack.HotModuleReplacementPlugin(), // Auto refresh page
   ],
 }
 
-const complier = webpack(webpackconfig)
-
-/* eslint-disable */
-complier.run((err, stats) => {
-  console.log(err)
-  console.log(stats)
+new WebpackDevServer(webpack(webpackconfig), {
+  historyApiFallback: true, // Allows reloading of any URL
+  hot: true, // Auto refresh page
+  publicPath: webpackconfig.output.publicPath, // Public bath
+  quiet: false, // Hides Errors
+  stats: {
+    chunks: false, // Hides the build chunks
+    colors: true, // Colors the output
+  },
+  watchOptions: {
+    ignored: /node_modules/, // Don't hot reload node modules
+  },
+}).listen(3000, (err, result) => {
+  if (err) {
+    console.log(err, result) //eslint-disable-line
+  }
+  console.log('Starting the development server on port 3000 👌') //eslint-disable-line
 })
-/* eslint-enable */
